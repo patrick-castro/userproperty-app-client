0277daf204ad72e050f06b622652715f249de27c
patrick-castro <castropatrickantonio@gmail.com>
2021-04-12 19:48:56 +0800

    Updated the URI of the Apollo Client and added proxy to package.json

--- src/AppConfig.js
+++ src/AppConfig.js
@@ -8,8 +8,7 @@ import App from './App';
 import reducers from './reducers';
 
 const client = new ApolloClient({
-  // uri: 'http://localhost:4000/graphql', // Local development
-  uri: 'https://userproperties-app-server.herokuapp.com/',
+  uri: '/graphql',
   cache: new InMemoryCache(),
 });
 
